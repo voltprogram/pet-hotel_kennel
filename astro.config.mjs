759036f9f763c25032fcabcf62f909b6777c797e
masaki kazama <103808505+voltprogram@users.noreply.github.com>
2025-11-30 02:53:59 +0900

    Add Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://voltprogram.github.io',
+  base: '/pet-hotel_kennel',
+});
